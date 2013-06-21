c603a75903ee0081c14d64d8dab2b95999733edc
maxibarros@gmail.com <maxibarros@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-06-21 14:57:11 +0000

    Implementada interfaz de validaciones.

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/interfaces/Validator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.interfaces;
+
+import tesis.odontologia.core.exception.GenericException;
+
+/**
+ *
+ * @author Maxi
+ */
+public interface Validator {
+    
+    void validar() throws GenericException;
+    
+}
