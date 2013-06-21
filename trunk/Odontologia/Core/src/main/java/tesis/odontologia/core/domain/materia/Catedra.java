1a819038c8b10c40fdbc81c56494bb72f948efa6
aspesot@gmail.com <aspesot@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-06-21 16:21:14 +0000

    Se agregó la clase catedra y se modificó materia

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Catedra.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.domain.materia;
+
+import tesis.odontologia.core.domain.Generic;
+import tesis.odontologia.core.exception.CatedraException;
+import tesis.odontologia.core.exception.GenericException;
+
+/**
+ *
+ * @author alespe
+ */
+public class Catedra extends Generic {
+
+    private String nombre;
+
+    public Catedra() {
+    }
+
+    public Catedra(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void validar() throws GenericException {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new CatedraException("El nombre de la cátedra no puede ser nulo o vacio.");
+        }
+    }
+}
