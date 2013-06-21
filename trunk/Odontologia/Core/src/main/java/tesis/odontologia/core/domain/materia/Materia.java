8e0570a2fbe834082aa3c80125b4f3120f230e4e
aspesot@gmail.com <aspesot@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-06-21 14:29:42 +0000

    Se agregó la clase materia

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Materia.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.domain.materia;
+
+import tesis.odontologia.core.domain.Generic;
+import tesis.odontologia.core.exception.MateriaException;
+
+/**
+ *
+ * @author alespe
+ */
+public class Materia extends Generic {
+
+    private String nombre;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    void validar() throws Exception {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new MateriaException("El nombre de la materia no puede ser nulo o vacio.");
+        }
+    }
+}
