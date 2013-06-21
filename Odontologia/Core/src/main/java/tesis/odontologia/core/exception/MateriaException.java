360d5f3201e49899e1acc6e2791b4a77e69cb5cd
maxibarros <maxibarros@gmail.com>
2013-06-21 14:55:07 +0000

    Definida clase generica para las excepciones.

--- Odontologia/Core/src/main/java/tesis/odontologia/core/exception/MateriaException.java
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/exception/MateriaException.java
@@ -8,7 +8,7 @@ package tesis.odontologia.core.exception;
  *
  * @author Maxi
  */
-public class MateriaException extends Exception {
+public class MateriaException extends GenericException {
 
     public MateriaException(String message) {
         super(message);
