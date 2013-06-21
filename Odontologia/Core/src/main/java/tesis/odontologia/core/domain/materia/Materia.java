a14bdfb719fa89b2e7e759d0b1bb46f0bf6eba0a
aspesot <aspesot@gmail.com>
2013-06-21 16:21:14 +0000

    Se agregó la clase catedra y se modificó materia

--- Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Materia.java
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Materia.java
@@ -4,6 +4,8 @@
  */
 package tesis.odontologia.core.domain.materia;
 
+import java.util.List;
+import javax.persistence.OneToMany;
 import tesis.odontologia.core.domain.Generic;
 import tesis.odontologia.core.exception.GenericException;
 import tesis.odontologia.core.exception.MateriaException;
@@ -15,12 +17,15 @@ import tesis.odontologia.core.exception.MateriaException;
 public class Materia extends Generic {
 
     private String nombre;
+    @OneToMany
+    private List<Catedra> catedra;
 
     public Materia() {
     }
 
-    public Materia(String nombre) {
+    public Materia(String nombre, List<Catedra> catedra) {
         this.nombre = nombre;
+        this.catedra = catedra;
     }
 
     public String getNombre() {
@@ -30,11 +35,26 @@ public class Materia extends Generic {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+        public List<Catedra> getCatedra() {
+        return catedra;
+    }
+
+    public void setCatedra(List<Catedra> catedra) {
+        this.catedra = catedra;
+    }
+    
 
     @Override
     public void validar() throws GenericException {
         if (nombre == null || nombre.isEmpty()) {
             throw new MateriaException("El nombre de la materia no puede ser nulo o vacio.");
+        }
+        if (catedra == null) {
+            throw new MateriaException("El nombre de la catedra no puede ser nulo o vacio.");
+        }
+        
+        for(Catedra c : catedra){
+            c.validar();
         }
     }
 }
---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/exception/CatedraException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.exception;
+
+/**
+ *
+ * @author Maxi
+ */
+public class CatedraException extends GenericException {
+
+    public CatedraException(String message) {
+        super(message);
+    }
+}
