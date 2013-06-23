9df994b05414116a5a620ed29c4247ee7d0ab867
ilopezarzuaga@gmail.com <ilopezarzuaga@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-06-23 23:47:07 +0000

    Modificación del formPacienteBean para que busque un paciente a partir del número de documento

--- trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/FormPacienteBean.java
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/FormPacienteBean.java
@@ -4,6 +4,7 @@
  */
 package tesis.odontologia.interfaces.paciente;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -27,6 +28,20 @@ public class FormPacienteBean {
     private List<Paciente.EstadoCivilTipo> listaEstadoCivilTipo;
     //Lita de EstudiosTipo
     private List<Paciente.EstudiosTipo> listaEstudioTipo;
+    private List<Paciente> pacientes;
+    private String docBusqueda;
+
+    public String getDocBusqueda() {
+        return docBusqueda;
+    }
+
+    public void setDocBusqueda(String docBusqueda) {
+        this.docBusqueda = docBusqueda;
+    }
+
+    public List<Paciente> getPacientes() {
+        return pacientes;
+    }
 
     public void setPaciente(Paciente p) {
         this.paciente = p;
@@ -50,18 +65,61 @@ public class FormPacienteBean {
         listaEstudioTipo = Arrays.asList(Paciente.EstudiosTipo.values());
         return listaEstudioTipo;
     }
-    
-    public String navigate(){
+
+    public String navigate() {
         return "showPaciente";
     }
-    
+
     @PostConstruct
-    public void init(){
-        paciente = new Paciente("Javier", "López");
-        paciente.setDocumento(new Documento());
-        paciente.setDomicilio(new Domicilio());
+    public void init() {
+        
+        if(paciente == null){
+            paciente = new Paciente();
+            paciente.setDomicilio(new Domicilio());
+            paciente.setDocumento(new Documento());
+        }
+            
+        pacientes = new ArrayList<Paciente>();
+        pacientes.addAll(construirLista());
     }
-    
+
+    private List<Paciente> construirLista() {
+        List<Paciente> lista = new ArrayList<Paciente>();
+        Paciente p1 = new Paciente();
+        p1.setDocumento(new Documento());
+        p1.getDocumento().setNumero("123");
+        p1.setDomicilio(new Domicilio());
+        p1.setApellido("Jojo");
+
+        Paciente p2 = new Paciente();
+        p2.setDocumento(new Documento());
+        p2.getDocumento().setNumero("456");
+        p2.setDomicilio(new Domicilio());
+        p2.setApellido("Jeje");
+
+        Paciente p3 = new Paciente();
+        p3.setDocumento(new Documento());
+        p3.getDocumento().setNumero("789");
+        p3.setDomicilio(new Domicilio());
+        p3.setApellido("Jiji");
+        
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+
+        return lista;
+    }
+
+    public String buscarPaciente() {
+        for (Paciente p : pacientes) {
+            if (p.getDocumento().getNumero().equals(docBusqueda)==true) {
+                paciente = p;
+                break;
+            }
+        }
+        return "formPaciente";
+    }
+
     /**
      * Creates a new instance of FormPacienteBean
      */
