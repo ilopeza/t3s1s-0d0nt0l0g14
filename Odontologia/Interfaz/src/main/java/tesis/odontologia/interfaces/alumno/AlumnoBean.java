63154f7b703a59f46e28f2ea004043ceb9d9817d
aspesot <aspesot@gmail.com>
2013-06-25 03:09:08 +0000

    Se agregó formAlumno.xhtml y se modificó AlumnoBean.java

--- Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/alumno/AlumnoBean.java
+++ Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/alumno/AlumnoBean.java
@@ -4,40 +4,75 @@
  */
 package tesis.odontologia.interfaces.alumno;
 
+
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import tesis.odontologia.core.domain.Documento;
 import tesis.odontologia.core.domain.alumno.Alumno;
+
+
 
 /**
  *
  * @author alespe
  */
-
-@ManagedBean
+@ManagedBean(name = "formAlumnoBean")
 @SessionScoped
 
 public class AlumnoBean {
-    private Alumno nuevoAlumno= new Alumno();
-    
+
+    private Alumno alumno;
+    private List<Documento.TipoDocumento> listaTipoDocumento;
+
     public AlumnoBean() {
-        
     }
-        public void savePaciente(){
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno Alumno) {
+        this.alumno = Alumno;
+    }
+
+    public List<Documento.TipoDocumento> getListaTipoDocumento() {
+        listaTipoDocumento = Arrays.asList(Documento.TipoDocumento.values());
+        return listaTipoDocumento;
+    }
+
+    public String navigate() {
+        return "showAlumno";
+    }
+
+    @PostConstruct
+    public void init() {
+        
+        if(alumno == null){
+            alumno = new Alumno();
+            alumno.setDocumento(new Documento());
+        }
+
+    }
+    
+    
+    public void savePaciente() {
         //AlumnoService.create(nuevoAlumno);
     }
-    
-    public void updatePaciente(){
+
+    public void updatePaciente() {
         //AlumnoService.update(nuevoAlumno);
     }
-    
-    public void deletePaciente(){
+
+    public void deletePaciente() {
         //AlumnoService.delete(nuevoAlumno);
     }
-    
-    public Alumno searchAlumno(){
+
+    public Alumno searchAlumno() {
         Alumno alu = new Alumno();
         //AlumnoService.searchAlumno(alu);
         return alu;
     }
-    
 }
