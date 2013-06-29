ca6f5d247347cd0936026ac40f7fecf7645336fe
maxibarros <maxibarros@gmail.com>
2013-06-29 17:48:37 +0000

    Agregados test de prueba de metodos de los services genericos.

--- Odontologia/Core/src/test/java/tesis/odontologia/core/PersonaTest.java
+++ Odontologia/Core/src/test/java/tesis/odontologia/core/PersonaTest.java
@@ -1,5 +1,6 @@
 package tesis.odontologia.core;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 import tesis.odontologia.core.dao.PersonaDao;
@@ -8,6 +9,7 @@ import tesis.odontologia.core.domain.paciente.Paciente;
 import static org.testng.Assert.*;
 import tesis.odontologia.core.domain.Documento;
 import tesis.odontologia.core.domain.paciente.Domicilio;
+import tesis.odontologia.core.service.PersonaService;
 import tesis.odontologia.core.specification.PacienteSpecs;
 
 /**
@@ -16,7 +18,7 @@ import tesis.odontologia.core.specification.PacienteSpecs;
 public class PersonaTest extends AbstractTest {
 
     @Autowired
-    private PersonaDao personaDao;
+    private PersonaService personaService;
     Paciente p;
 
     @Test
@@ -24,14 +26,45 @@ public class PersonaTest extends AbstractTest {
         p = new Paciente("Maximiliano", "Barros");
         p.setDocumento(new Documento("34688417", Documento.TipoDocumento.DNI));
         p.setDomicilio(new Domicilio("Macaon 4123", "Cordoba"));
-        personaDao.save(p);
+        personaService.save(p);
     }
 
     @Test(dependsOnMethods = "crearPaciente")
     public void buscarPaciente() {
-        Persona per = (Persona) personaDao.findOne(PacienteSpecs.byNombre("Maximiliano"));
+        Persona per = (Persona) personaService.findOne(PacienteSpecs.byNombre("Maximiliano"));
         assertNotNull(per);
         System.out.println("Nombre: " + per.getNombre());
         System.out.println("Apellido: " + per.getApellido());
+    }
+    
+    @Test(dependsOnMethods = "buscarPaciente")
+    public void guardarConService() {
+        Paciente pa = new Paciente("Roberto", "Rodriguez");
+        pa.setDocumento(new Documento("17267218", Documento.TipoDocumento.DNI));
+        pa.setDomicilio(new Domicilio("Ruelle 4123", "Cordoba"));
+        personaService.save(pa);
+    }
+    
+    @Test(dependsOnMethods = "guardarConService")
+    public void testCount() {
+        Long count = personaService.count();
+        assertTrue(count > 0);
+        assertNotNull(count);
+        System.out.println("Count: " + count);
+    }
+    
+    @Test(dependsOnMethods = "guardarConService")
+    public void testFindAll() {
+        List<Persona> findAll = personaService.findAll();
+        assertNotNull(findAll);
+        assertFalse(findAll.isEmpty());
+        System.out.println("FindAll: " + findAll);
+    }
+    
+    @Test(dependsOnMethods = "guardarConService")
+    public void testSpecification() {
+        Persona findOne = personaService.findOne(PacienteSpecs.byNombre("robert"));
+        assertNotNull(findOne);
+        System.out.println("FinOne Predicate: " + findOne);
     }
 }
