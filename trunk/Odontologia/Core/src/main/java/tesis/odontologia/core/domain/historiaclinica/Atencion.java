3fba7650517e80976dc99a48cb23d5080dbecc81
maxibarros@gmail.com <maxibarros@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-05 15:10:16 +0000

    Se agrego el diagnostico obligatorio a la atencion.

--- trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/domain/historiaclinica/Atencion.java
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/domain/historiaclinica/Atencion.java
@@ -5,12 +5,15 @@
 package tesis.odontologia.core.domain.historiaclinica;
 
 import java.util.Calendar;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import tesis.odontologia.core.domain.Generic;
 import tesis.odontologia.core.exception.AtencionException;
@@ -28,19 +31,28 @@ public abstract class Atencion extends Generic {
     @Column(nullable = false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Calendar fechaAtencion;
+    
     @Column(nullable = false, length = 700)
     private String motivoConsultaOdontologica;
+    
     @Column(length = 255)
     private String comoComenzo;
+    
     @Column(length = 255)
     private String cuantoTiempoHace;
+    
     @Column(length = 255)
     private String donde;
+    
     @Column(length = 255)
     private String aQueLoAtribuye;
+    
     @Column(length = 255)
     private String queHizo;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "diagnostico_id")
+    private Diagnostico diagnostico;
     
     //CONSTRUCTORS
     public Atencion() {
@@ -57,6 +69,14 @@ public abstract class Atencion extends Generic {
     }
     
     //GETTERS AND SETTERS
+    public Diagnostico getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(Diagnostico diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    
     public Calendar getFechaAtencion() {
         return fechaAtencion;
     }
@@ -121,6 +141,11 @@ public abstract class Atencion extends Generic {
         }
         if(motivoConsultaOdontologica == null || motivoConsultaOdontologica.isEmpty()){
             throw new AtencionException("El motivo de consulta odontologica no puede ser nulo o vacio");
+        }
+        if(diagnostico == null) {
+            throw new AtencionException("El diagnostico atendido no puede ser nulo.");
+        } else {
+            diagnostico.validar();
         }
     }
     
