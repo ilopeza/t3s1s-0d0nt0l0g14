bdb3d6882ba9beeebd3f4e3ecd3f3f9e01b16b3d
enzo.biancato@gmail.com <enzo.biancato@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-13 15:08:43 +0000

    Modificado el Bean Alumno para que se ajuste al nuevo formAlumno

--- trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/alumno/AlumnoBean.java
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/alumno/AlumnoBean.java
@@ -9,9 +9,12 @@ import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import tesis.odontologia.core.domain.Documento;
 import tesis.odontologia.core.domain.alumno.Alumno;
+import tesis.odontologia.core.domain.usuario.Rol;
+import tesis.odontologia.core.domain.usuario.Usuario;
+import tesis.odontologia.core.mail.SMTPConfig;
 
 
 
@@ -20,12 +23,13 @@ import tesis.odontologia.core.domain.alumno.Alumno;
  * @author alespe
  */
 @ManagedBean(name = "formAlumnoBean")
-@SessionScoped
+@ViewScoped
 
 public class AlumnoBean {
 
     private Alumno alumno;
     private List<Documento.TipoDocumento> listaTipoDocumento;
+    private String datosAlumnoRegistrado;
 
     public AlumnoBean() {
     }
@@ -43,9 +47,24 @@ public class AlumnoBean {
         return listaTipoDocumento;
     }
 
-    public String navigate() {
-        return "showAlumno";
+    public void registrarAlumno() {
+        String password = "pass"; //se genera la clave
+        Rol rol = new Rol("Alumno"); //todos los roles deberian estar generados.
+        Usuario usuario = new Usuario(); //se genera el usuario
+        usuario.setUsuario(alumno.getEmail()); //se asigna el email como nombre de usuario.
+        usuario.setContraseña(password); //se setea la contraseña generada.
+        usuario.setRol(rol);
+        alumno.setUsuario(usuario);
+        datosAlumnoRegistrado = "Se registró el alumno con los siguientes datos:" + "\nUsuario: " + alumno.getEmail() + "\nContraseña: " + alumno.getUsuario().getContraseña();
+        SMTPConfig.sendMail("Registro en SAPO", "Te has registrado en el sistema SAPO, que te permite buscar pacientes para tus practicas. Tus datos de inicio de sesión son los siguientes: Usuario, Contraseña. Hace click en el siguiente enlace para completar tu registro.", alumno.getEmail());
     }
+    
+    public String datosAlumnoRegistrado(){
+        
+      return datosAlumnoRegistrado;
+    }
+    
+   
 
     @PostConstruct
     public void init() {
