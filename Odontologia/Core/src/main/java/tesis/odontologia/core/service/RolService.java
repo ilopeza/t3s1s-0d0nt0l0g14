3ce57ea7c762eb611738aef08df8953f07fa58b7
enzo.biancato <enzo.biancato@gmail.com>
2013-07-26 14:04:09 +0000

    Se creo la clase RolService

---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/service/RolService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.service;
+
+import tesis.odontologia.core.domain.usuario.Rol;
+
+
+
+/**
+ *
+ * @author Maxi
+ */
+public interface RolService extends GenericService<Rol> {
+    
+
+}
