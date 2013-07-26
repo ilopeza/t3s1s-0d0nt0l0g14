b1a44ffd6db526d53e7af7c860e5d8faff331dac
maxibarros <maxibarros@gmail.com>
2013-07-26 14:08:12 +0000

    Se quitaron los trabajos practicos de las catedras (provisoriamente no aplican al proyecto).

--- Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Catedra.java
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/domain/materia/Catedra.java
@@ -21,10 +21,10 @@ import tesis.odontologia.core.exception.GenericException;
 public class Catedra extends Generic {
 
     private String nombre;
-    
-    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
-    @JoinColumn(name = "materia_id")
-    private List<TrabajoPractico> trabajoPractico;
+//    
+//    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "materia_id")
+//    private List<TrabajoPractico> trabajoPractico;
 
     public Catedra() {
     }
