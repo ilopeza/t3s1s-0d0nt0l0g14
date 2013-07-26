dd9249f38fd8add197d2d278700533039f23f00d
ilopezarzuaga@gmail.com <ilopezarzuaga@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-26 13:37:45 +0000

    Se agrega la clase Utiles para operaciones comunes.

---
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/util/Utiles.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.interfaces.util;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class Utiles {
+
+    /**
+     * Convierte una cadena en un objeto Integer.
+     *
+     * @param original String a convertir
+     * @return Integer convertido
+     */
+    public static Integer convertStringToInt(String original) throws NumberFormatException{
+        Integer conv = null;
+        conv = Integer.parseInt(original);
+        return conv;
+    }
+
+    public static Calendar convertIntegerToCalendarYear(Integer year){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year.intValue());
+        
+        return c;
+    }
+}
