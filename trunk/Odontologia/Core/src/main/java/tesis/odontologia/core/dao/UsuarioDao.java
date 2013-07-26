cdfabf1b531bf9dcaf372a3848af7326065d83c0
enzo.biancato@gmail.com <enzo.biancato@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-26 14:03:36 +0000

    Se creo la clase UsuarioDao

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/dao/UsuarioDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.dao;
+
+import tesis.odontologia.core.domain.usuario.Usuario;
+
+
+/**
+ *
+ * @author Maxi
+ */
+public interface UsuarioDao extends GenericDao<Usuario, Long> {
+}
