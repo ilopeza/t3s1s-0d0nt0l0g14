ea577ec784a102bed015151793631e9d1d62fb77
enzo.biancato <enzo.biancato@gmail.com>
2013-07-26 14:04:40 +0000

    Se creo la clase UsuarioService

---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/service/UsuarioService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.service;
+
+import tesis.odontologia.core.domain.usuario.Usuario;
+
+
+
+/**
+ *
+ * @author Maxi
+ */
+public interface UsuarioService extends GenericService<Usuario> {
+    
+
+}
