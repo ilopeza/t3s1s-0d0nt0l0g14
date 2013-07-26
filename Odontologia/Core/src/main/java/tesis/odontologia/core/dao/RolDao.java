8495a0f253bfe1978d3da00e0d6b885819a15912
enzo.biancato <enzo.biancato@gmail.com>
2013-07-26 14:03:06 +0000

    Se creo la clase RolDao

---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/dao/RolDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.dao;
+
+import tesis.odontologia.core.domain.usuario.Rol;
+
+/**
+ *
+ * @author Maxi
+ */
+public interface RolDao extends GenericDao<Rol, Long> {
+}
