e27373ba188abf63beb30594ddacd2308645bed2
ilopezarzuaga@gmail.com <ilopezarzuaga@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-27 19:29:29 +0000

    Se hicieron cambios menores en BuscarTableBean (probablemente haya que borrarlo) y FormPacienteBean

--- trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/BuscarTableBean.java
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/BuscarTableBean.java
@@ -4,14 +4,22 @@
  */
 package tesis.odontologia.interfaces.paciente;
 
+import com.mysema.query.types.Predicate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import tesis.odontologia.core.domain.materia.Materia;
 import tesis.odontologia.core.domain.materia.TrabajoPractico;
 import tesis.odontologia.core.domain.paciente.Paciente;
+import tesis.odontologia.core.service.PersonaService;
+import tesis.odontologia.core.specification.PacienteSpecs;
+import tesis.odontologia.core.specification.PersonaSpecs;
+import tesis.odontologia.interfaces.util.Utiles;
 
 /**
  *
@@ -30,17 +38,21 @@ public class BuscarTableBean {
     private String apellido;
     private static String[] nombres;
     private static String[] apellidos;
+    //Atributos para la búsqueda simple
+    private String nroDocumentoFiltro;
     // Atributos usados para la búsqueda avanzada.
     private List<Materia> materias;
     private Materia materiaFiltro;
     private List<TrabajoPractico> practicos;
     private TrabajoPractico practicoFiltro;
-    private int edadDesdeFiltro;
-    private int edadHastaFiltro;
+    private String edadDesdeFiltro;
+    private String edadHastaFiltro;
     private String nombreFiltro;
-
     //private List<Enfermedad> enfermedades;
     //private Enfermedad enfermedadFiltro;
+    @ManagedProperty(value = "#{personaService}")
+    private PersonaService personaService;
+
     static {
 
         nombres = (new String[5]);
@@ -61,17 +73,21 @@ public class BuscarTableBean {
 
 
     }
-
-    public void cargarLista(List<Paciente> list, int size) {
-        for (int i = 0; i < size; i++) {
-            list.add(new Paciente(nombres[i], apellidos[i]));
-        }
+    
+    public void setPersonaService(PersonaService personaService) {
+        this.personaService = personaService;
     }
+
+//    public void cargarLista(List<Paciente> list, int size) {
+//        for (int i = 0; i < size; i++) {
+//            list.add(new Paciente(nombres[i], apellidos[i]));
+//        }
+//    }
 
     @PostConstruct
     public void init() {
         pacientes = new ArrayList<Paciente>();
-        cargarLista(pacientes, 5);
+//        //cargarLista(pacientes, 5);
         materias = new ArrayList();
         materias.addAll(cargarMaterias());
 
@@ -94,18 +110,13 @@ public class BuscarTableBean {
         this.materias = materias;
     }
 
-    public Materia getMateriaElegida() {
-        return materiaFiltro;
-    }
-
-    public void setMateriaElegida(Materia materiaElegida) {
-        this.materiaFiltro = materiaElegida;
-    }
+    
 
     /**
      * @return the paciente
      */
     public Paciente getPaciente() {
+        
         return paciente;
     }
 
@@ -120,6 +131,9 @@ public class BuscarTableBean {
      * @return the pacientes
      */
     public List<Paciente> getPacientes() {
+        Predicate p = null;
+        p = PersonaSpecs.byClass(Paciente.class);
+        pacientes = (List<Paciente>)personaService.findAll(p);
         return pacientes;
     }
 
@@ -195,6 +209,15 @@ public class BuscarTableBean {
         this.pacienteSeleccionado = pacienteSeleccionado;
     }
 
+    //Métodos getter y setter para la búsqueda simple.
+    public String getNroDocumentoFiltro() {
+        return nroDocumentoFiltro;
+    }
+
+    public void setNroDocumentoFiltro(String nroDocumentoFiltro) {
+        this.nroDocumentoFiltro = nroDocumentoFiltro;
+    }
+
     //Métodos getter y setter para la búsqueda avanzada.
     public Materia getMateriaFiltro() {
         return materiaFiltro;
@@ -220,19 +243,19 @@ public class BuscarTableBean {
         this.practicoFiltro = practicoFiltro;
     }
 
-    public int getEdadDesdeFiltro() {
+    public String getEdadDesdeFiltro() {
         return edadDesdeFiltro;
     }
 
-    public void setEdadDesdeFiltro(int edadDesdeFiltro) {
+    public void setEdadDesdeFiltro(String edadDesdeFiltro) {
         this.edadDesdeFiltro = edadDesdeFiltro;
     }
 
-    public int getEdadHastaFiltro() {
+    public String getEdadHastaFiltro() {
         return edadHastaFiltro;
     }
 
-    public void setEdadHastaFiltro(int edadHastaFiltro) {
+    public void setEdadHastaFiltro(String edadHastaFiltro) {
         this.edadHastaFiltro = edadHastaFiltro;
     }
 
@@ -244,17 +267,72 @@ public class BuscarTableBean {
         this.nombreFiltro = nombreFiltro;
     }
 
+
+    public String buscarPacientes() {
+        pacientes.clear();
+        if (nroDocumentoFiltro != null && nroDocumentoFiltro.length() > 0) {
+            pacientes.add(busquedaSimple());
+        } else {
+            pacientes.addAll(busquedaAvanzada());
+        }
+        return "buscarPaciente";
+    }
+
+    // Métodos auxiliares.
+    private Collection<? extends Paciente> busquedaAvanzada() {
+        Predicate p = null;
+        if (edadDesdeFiltro == null) {
+            // Busca pacientes que tengan como máximo cierta edad.
+            p = PacienteSpecs.byMenorA(convertirFechaHasta());
+        } else {
+            if (edadHastaFiltro == null) {
+                //Busca pacientes que hayan nacido a partir de cierto año.
+                p = PacienteSpecs.byMayorA(convertirFechaDesde());
+            } else {
+                p = PacienteSpecs.byEdadEntre(convertirFechaDesde(), convertirFechaHasta());
+            }
+        }
+        if (!materiaFiltro.getNombre().equalsIgnoreCase("Ninguno")) {
+            p = PacienteSpecs.byDiagnosticoMateria(materiaFiltro).and(p);
+        }
+
+        pacientesFiltrados = (List<Paciente>) personaService.findAll(p);
+
+        return pacientesFiltrados;
+    }
+
+    private Paciente busquedaSimple() {
+        return personaService.findOne(PacienteSpecs.byNumeroDocumento(nroDocumentoFiltro));
+    }
+
+    private Calendar convertirFechaDesde() {
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+        int anioDesde = anioActual - Utiles.convertStringToInt(edadDesdeFiltro).intValue();
+
+        return Utiles.convertIntegerToCalendarYear(anioDesde);
+    }
+
+    private Calendar convertirFechaHasta() {
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+        int anioHasta = anioActual - Utiles.convertStringToInt(edadHastaFiltro).intValue();
+
+        return Utiles.convertIntegerToCalendarYear(anioHasta);
+    }
+
     public List<Materia> cargarMaterias() {
         List<Materia> lista = new ArrayList<Materia>();
 
         Materia m1 = new Materia();
         Materia m2 = new Materia();
         Materia m3 = new Materia();
-
+        Materia m= new Materia();
+        
+        m.setNombre("Ninguno");
         m1.setNombre("Prostoconcia");
         m2.setNombre("Endodoncia");
         m3.setNombre("Ortodoncia");
 
+        lista.add(m);
         lista.add(m1);
         lista.add(m2);
         lista.add(m3);
