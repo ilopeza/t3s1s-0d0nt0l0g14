89d9c0ce6df707d1219982ae5563ce77deb390de
aspesot <aspesot@gmail.com>
2013-07-28 18:01:26 +0000

    Se agregó AsignacionPacienteDao.java

---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/dao/AsignacionPacienteDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.dao;
+
+import tesis.odontologia.core.domain.asignaciones.AsignacionPaciente;
+
+
+/**
+ *
+ * @author Maxi
+ */
+public interface AsignacionPacienteDao extends GenericDao<AsignacionPaciente, Long> {
+}
