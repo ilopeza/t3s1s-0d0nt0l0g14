b028b278bd1f74322166d04e2c9f42cbc7bc3ef4
aspesot <aspesot@gmail.com>
2013-07-28 18:02:48 +0000

    Se agregó AsignacionPacienteException.java

---
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/exception/AsignacionPacienteException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.exception;
+
+/**
+ *
+ * @author Maxi
+ */
+public class AsignacionPacienteException extends GenericException {
+
+    public AsignacionPacienteException(String message) {
+        super(message);
+    }
+}
