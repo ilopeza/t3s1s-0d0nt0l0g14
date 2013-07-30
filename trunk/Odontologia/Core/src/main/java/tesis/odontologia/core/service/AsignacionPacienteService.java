6608ab1d97de8df146f679c3b63b0f554ac816a7
aspesot@gmail.com <aspesot@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-07-30 00:16:30 +0000

    se creó AsignacionPacienteService.java

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/service/AsignacionPacienteService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.service;
+
+import tesis.odontologia.core.domain.asignaciones.AsignacionPaciente;
+
+/**
+ *
+ * @author Maxi
+ */
+public interface AsignacionPacienteService extends GenericService<AsignacionPaciente> {
+
+}
