692464dd032cf5f9d4a0b33e89c6ba388522530d
mau.g.sistemas <mau.g.sistemas@gmail.com>
2013-08-08 04:08:40 +0000

    Se agregan los componentes para poder buscar por documento y nombre. Se puede modificar los datos de un paciente

--- Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/FormPacienteBean.java
+++ Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/paciente/FormPacienteBean.java
@@ -4,9 +4,11 @@
  */
 package tesis.odontologia.interfaces.paciente;
 
+import com.mysema.query.types.Predicate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -17,10 +19,14 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.print.attribute.standard.Severity;
 import tesis.odontologia.core.domain.Documento;
+import tesis.odontologia.core.domain.materia.Materia;
 import tesis.odontologia.core.domain.paciente.Domicilio;
 import tesis.odontologia.core.domain.paciente.Paciente;
 import tesis.odontologia.core.exception.GenericException;
 import tesis.odontologia.core.service.PersonaService;
+import tesis.odontologia.core.specification.PacienteSpecs;
+import tesis.odontologia.core.specification.PersonaSpecs;
+import tesis.odontologia.interfaces.util.Utiles;
 
 /**
  *
@@ -31,6 +37,8 @@ import tesis.odontologia.core.service.PersonaService;
 public class FormPacienteBean {
 
     private Paciente paciente;
+    private Paciente pacienteEncontrado;
+    private Paciente pacienteSeleccionado;
     private List<Documento.TipoDocumento> listaTipoDocumento;
     //Lista de EstadoCivil
     private List<Paciente.EstadoCivilTipo> listaEstadoCivilTipo;
@@ -45,15 +53,19 @@ public class FormPacienteBean {
     private int estudia;
     // Para saber si tiene servicio de emergencia.
     private int servicio;
- 
-            
+    //Atributos búsqueda simple.
+    private String nroDocumentoFiltro;
+    //Atributos búsqueda avanzada.
+    private String edadDesdeFiltro;
+    private String edadHastaFiltro;
+    private String nombreFiltro;
     @ManagedProperty(value = "#{personaService}")
     private PersonaService personaService;
-    
+
     public int getEstudia() {
-        if(paciente != null && paciente.getTipoEstudios() != null){
+        if (paciente != null && paciente.getTipoEstudios() != null) {
             estudia = 1;
-        }else{
+        } else {
             estudia = 0;
         }
         return estudia;
@@ -61,37 +73,39 @@ public class FormPacienteBean {
 
     public void setEstudia(int estudia) {
         this.estudia = estudia;
-        if(estudia == 0){
+        if (estudia == 0) {
             paciente.setTipoEstudios(null);
         }
     }
 
     public int getServicio() {
-        if(paciente != null && paciente.getServicioEmergencia()!=null){
+        if (paciente != null && paciente.getServicioEmergencia() != null) {
             servicio = 1;
-        }else{
+        } else {
             servicio = 0;
         }
         return servicio;
     }
 
     public void setServicio(int servicio) {
-        
+
         this.servicio = servicio;
     }
 
     public Date getFechaNacimiento() {
-        return paciente.getFechaNacimiento() == null? null: paciente.getFechaNacimiento().getTime();
+        return paciente.getFechaNacimiento() == null ? null : paciente.getFechaNacimiento().getTime();
     }
 
     public void setFechaNacimiento(Date fechaNacimiento) {
-        if(fechaNacimiento == null) return;
+        if (fechaNacimiento == null) {
+            return;
+        }
         this.fechaNacimiento = fechaNacimiento;
         Calendar cal = Calendar.getInstance();
         cal.setTime(fechaNacimiento);
         paciente.setFechaNacimiento(cal);
     }
-    
+
     public String getDocBusqueda() {
         return docBusqueda;
     }
@@ -105,8 +119,8 @@ public class FormPacienteBean {
     }
 
     public void setPaciente(Paciente p) {
-        
-        
+
+
         this.paciente = p;
     }
 
@@ -130,61 +144,103 @@ public class FormPacienteBean {
     }
 
     public String save() {
-        paciente = personaService.save(paciente);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Paciente " + paciente.toString()+
-                " guardado correctamente."));
+        if (pacienteEncontrado != null) {
+            paciente = getPersonaService().save(pacienteEncontrado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Paciente " + paciente.toString()
+                    + " actualizado correctamente."));
+        } else {
+            paciente = getPersonaService().save(paciente);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Paciente " + paciente.toString()
+                    + " guardado correctamente."));
+
+        }
         return "formPaciente";
     }
 
     @PostConstruct
     public void init() {
-        
-        if(paciente == null){
+
+        if (paciente == null) {
             paciente = new Paciente();
             paciente.setDomicilio(new Domicilio());
             paciente.setDocumento(new Documento());
         }
-            
+
         pacientes = new ArrayList<Paciente>();
-        pacientes.addAll(construirLista());
+
     }
 
-    private List<Paciente> construirLista() {
-        List<Paciente> lista = new ArrayList<Paciente>();
-        Paciente p1 = new Paciente();
-        p1.setDocumento(new Documento());
-        p1.getDocumento().setNumero("123");
-        p1.setDomicilio(new Domicilio());
-        p1.setApellido("Jojo");
+    // Métodos de la interfaz.
+    public void buscarPacientes() {
+//        if (getNroDocumentoFiltro() == null || getNroDocumentoFiltro().isEmpty()) {
+//            if ((getEdadDesdeFiltro() == null || getEdadDesdeFiltro().isEmpty()) && (getEdadHastaFiltro().isEmpty() || getEdadHastaFiltro() == null)) {
+//                buscarTodosLosPacientes();
+//            } else {
+//                busquedaAvanzada();
+//            }
+//        } else {
+//            busquedaSimple();
+//        }
 
-        Paciente p2 = new Paciente();
-        p2.setDocumento(new Documento());
-        p2.getDocumento().setNumero("456");
-        p2.setDomicilio(new Domicilio());
-        p2.setApellido("Jeje");
-
-        Paciente p3 = new Paciente();
-        p3.setDocumento(new Documento());
-        p3.getDocumento().setNumero("789");
-        p3.setDomicilio(new Domicilio());
-        p3.setApellido("Jiji");
-        
-        
-        lista.add(p1);
-        lista.add(p2);
-        lista.add(p3);
-
-        return lista;
-    }
-
-    public String buscarPaciente() {
-        for (Paciente p : pacientes) {
-            if (p.getDocumento().getNumero().equals(docBusqueda)==true) {
-                paciente = p;
-                break;
+        if (getNroDocumentoFiltro() == null || getNroDocumentoFiltro().isEmpty()) {
+            if (getNombreFiltro() == null || getNombreFiltro().isEmpty()) {
+                buscarTodosLosPacientes();
+            } else {
+                busquedaAvanzada();
             }
+        } else {
+            busquedaSimple();
         }
-        return "formPaciente";
+
+    }
+
+    //Métodos auxiliares.
+    private void busquedaSimple() {
+        pacientes.clear();
+        Predicate p = PacienteSpecs.byNumeroDocumento(getNroDocumentoFiltro());
+        paciente = (Paciente) getPersonaService().findOne(p);
+        if (paciente != null) {
+            pacienteEncontrado = paciente;
+        }
+        pacientes.add(paciente);
+    }
+
+    private void busquedaAvanzada() {
+        pacientes.clear();
+        Predicate p = null;
+
+//        if (getEdadDesdeFiltro() != null && getEdadDesdeFiltro().length() > 0) {
+//            // Busca pacientes que tengan como máximo cierta edad.
+//            p = PacienteSpecs.byMayorA(convertirFechaDesde());
+//        }
+//        if (getEdadHastaFiltro() != null && getEdadHastaFiltro().length() > 0) {
+//            p = PacienteSpecs.byMenorA(convertirFechaHasta()).and(p);
+//        }
+
+        if (getNombreFiltro() != null && getNombreFiltro().length() > 0) {
+            p = PacienteSpecs.byNombre(nombreFiltro);
+        }
+
+        pacientes.addAll((Collection<? extends Paciente>) getPersonaService().findAll(p));
+    }
+
+    private void buscarTodosLosPacientes() {
+        Predicate p = PersonaSpecs.byClass(Paciente.class);
+        pacientes = (List<Paciente>) getPersonaService().findAll(p);
+    }
+
+    private Calendar convertirFechaDesde() {
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+        int anioDesde = anioActual - Utiles.convertStringToInt(getEdadDesdeFiltro()).intValue();
+
+        return Utiles.convertIntegerToCalendarYear(anioDesde);
+    }
+
+    private Calendar convertirFechaHasta() {
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+        int anioHasta = anioActual - Utiles.convertStringToInt(getEdadHastaFiltro()).intValue();
+
+        return Utiles.convertIntegerToCalendarYear(anioHasta);
     }
 
     /**
@@ -195,5 +251,82 @@ public class FormPacienteBean {
 
     public void setPersonaService(PersonaService personaService) {
         this.personaService = personaService;
+    }
+
+    /**
+     * @return the nroDocumentoFiltro
+     */
+    public String getNroDocumentoFiltro() {
+        return nroDocumentoFiltro;
+    }
+
+    /**
+     * @param nroDocumentoFiltro the nroDocumentoFiltro to set
+     */
+    public void setNroDocumentoFiltro(String nroDocumentoFiltro) {
+        this.nroDocumentoFiltro = nroDocumentoFiltro;
+    }
+
+    /**
+     * @return the edadDesdeFiltro
+     */
+    public String getEdadDesdeFiltro() {
+        return edadDesdeFiltro;
+    }
+
+    /**
+     * @param edadDesdeFiltro the edadDesdeFiltro to set
+     */
+    public void setEdadDesdeFiltro(String edadDesdeFiltro) {
+        this.edadDesdeFiltro = edadDesdeFiltro;
+    }
+
+    /**
+     * @return the edadHastaFiltro
+     */
+    public String getEdadHastaFiltro() {
+        return edadHastaFiltro;
+    }
+
+    /**
+     * @param edadHastaFiltro the edadHastaFiltro to set
+     */
+    public void setEdadHastaFiltro(String edadHastaFiltro) {
+        this.edadHastaFiltro = edadHastaFiltro;
+    }
+
+    /**
+     * @return the personaService
+     */
+    public PersonaService getPersonaService() {
+        return personaService;
+    }
+
+    /**
+     * @return the nombreFiltro
+     */
+    public String getNombreFiltro() {
+        return nombreFiltro;
+    }
+
+    /**
+     * @param nombreFiltro the nombreFiltro to set
+     */
+    public void setNombreFiltro(String nombreFiltro) {
+        this.nombreFiltro = nombreFiltro;
+    }
+
+    /**
+     * @return the pacienteSeleccionado
+     */
+    public Paciente getPacienteSeleccionado() {
+        return pacienteSeleccionado;
+    }
+
+    /**
+     * @param pacienteSeleccionado the pacienteSeleccionado to set
+     */
+    public void setPacienteSeleccionado(Paciente pacienteSeleccionado) {
+        this.pacienteSeleccionado = pacienteSeleccionado;
     }
 }
