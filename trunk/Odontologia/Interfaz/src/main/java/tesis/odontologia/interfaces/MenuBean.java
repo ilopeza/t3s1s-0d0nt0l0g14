9095c7126ea69ae62d5a130c80f58b9f3ebaac74
maxibarros@gmail.com <maxibarros@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-09-10 21:45:54 +0000

    Se modifico la interfaz para mover el menu del lado izquierdo como una barra de menu horizontal.

---
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/MenuBean.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.interfaces;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import org.primefaces.component.menuitem.MenuItem;
+import org.primefaces.component.submenu.Submenu;
+import org.primefaces.model.DefaultMenuModel;
+import org.primefaces.model.MenuModel;
+
+
+/**
+ *
+ * @author Maxi
+ */
+@ManagedBean
+@SessionScoped
+public class MenuBean {
+
+    /**
+     * Creates a new instance of MenuBean
+     */
+    private MenuModel menu = new DefaultMenuModel();;
+    
+    public MenuBean() {
+        Submenu submenu;
+        MenuItem menuItem;
+        //Menu
+        submenu = subMenu("Gestión de Pacientes");
+        //Opciones
+        menuItem = menuItem("Registrar Paciente","/pages/pacientes/formPaciente.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+        
+        //Menu
+        submenu = subMenu("Gestión de Materias");
+        //Opciones
+        menuItem = menuItem("Registrar Materia","/pages/materias/formMaterias.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+        
+        //Menu
+        submenu = subMenu("Gestión de Alumnos");
+        //Opciones
+        menuItem = menuItem("Registrar Alumno","/pages/alumnos/formAlumno.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+        
+        //Menu
+        submenu = subMenu("Gestión de Usuarios");
+        //Opciones
+        menuItem = menuItem("Registrar Usuario de Alumno","/pages/alumnos/formUsuarioAlumno.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+        
+        //Menu
+        submenu = subMenu("Asignaciones de Pacientes");
+        //Opciones
+        menuItem = menuItem("Asignacion Paciente","/pages/asignaciones/asignacionPaciente.xhtml");
+        submenu.getChildren().add(menuItem);
+        menuItem = menuItem("Consultar Asignacion Paciente","/pages/asignaciones/consultarAsignacionesPaciente.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+        //Menu
+        submenu = subMenu("Cuenta");
+        //Opciones
+        menuItem = menuItem("Recuperar Password","/pages/usuario/recuperarPassword.xhtml");
+        submenu.getChildren().add(menuItem);
+        menu.addSubmenu(submenu);
+    }
+
+    public MenuModel getMenu() {
+        return menu;
+    }
+
+    public void setMenu(MenuModel menu) {
+        this.menu = menu;
+    }
+    
+    
+    private Submenu subMenu(String label) {
+        Submenu submenu = new Submenu();
+        submenu.setLabel(label);
+        return submenu;
+    }
+    
+    private MenuItem menuItem(String value, String url) {
+        MenuItem menuItem = new MenuItem();
+        menuItem.setValue(value);
+        menuItem.setUrl(url);
+        return menuItem;
+    }
+    
+}
