6213613c4fc711bde78b858a3fd5956f77e78449
aspesot@gmail.com <aspesot@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-09-16 22:19:58 +0000

    se agrego el paquete y la clase Profesor.java

---
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/domain/profesor/Profesor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesis.odontologia.core.domain.profesor;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import tesis.odontologia.core.domain.Persona;
+import tesis.odontologia.core.exception.GenericException;
+
+/**
+ *
+ * @author Maxi
+ */
+@Entity
+@DiscriminatorValue(value = "PROFESOR")
+public class Profesor extends Persona {
+
+    public Profesor() {
+    }
+
+    public Profesor(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+    @Override
+    public void validar() throws GenericException {
+        super.validar();
+    }
+
+}
