a248d06ce4a494a9166c416bcf4a4bb2c1648d10
aspesot@gmail.com <aspesot@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-09-18 01:03:23 +0000

    Se comentaron los test

--- trunk/Odontologia/Core/src/test/java/tesis/odontologia/core/PersonaTest.java
+++ trunk/Odontologia/Core/src/test/java/tesis/odontologia/core/PersonaTest.java
@@ -22,68 +22,68 @@ public class PersonaTest extends AbstractTest {
     private PersonaService personaService;
     Paciente p;
 
-    @Test
-    public void crearPaciente() {
-        p = new Paciente("Maximiliano", "Barros");
-        p.setDocumento(new Documento("34688417", Documento.TipoDocumento.DNI));
-        HistoriaClinica hc= HistoriaClinica.createDefault();
-        hc.setNumero(1);
-        p.setHistoriaClinica(hc);
-        p.setFechaNacimiento(Calendar.getInstance());
-//        p.setDomicilio(new Domicilio("Macaon 4123", "Cordoba"));
-        personaService.save(p);
-    }
-
-    @Test(dependsOnMethods = "crearPaciente")
-    public void buscarPaciente() {
-        Persona per = (Persona) personaService.findOne(PacienteSpecs.byNombre("Maximiliano"));
-        assertNotNull(per);
-        System.out.println("Nombre: " + per.getNombre());
-        System.out.println("Apellido: " + per.getApellido());
-    }
-    
-    @Test(dependsOnMethods = "buscarPaciente")
-    public void guardarConService() {
-        Paciente pa = new Paciente("Roberto", "Rodriguez");
-        pa.setDocumento(new Documento("17267218", Documento.TipoDocumento.DNI));
-        HistoriaClinica hc= HistoriaClinica.createDefault();
-        hc.setNumero(2);
-        pa.setHistoriaClinica(hc);
-//        pa.setDomicilio(new Domicilio("Ruelle 4123", "Cordoba"));
-        pa.setFechaNacimiento(Calendar.getInstance());
-        personaService.save(pa);
-    }
-    
-    @Test(dependsOnMethods = "guardarConService")
-    public void testCount() {
-        Long count = personaService.count();
-        assertTrue(count > 0);
-        assertNotNull(count);
-        System.out.println("Count: " + count);
-    }
-    
-    @Test(dependsOnMethods = "guardarConService")
-    public void testFindAll() {
-        List<Persona> findAll = personaService.findAll();
-        assertNotNull(findAll);
-        assertFalse(findAll.isEmpty());
-        System.out.println("FindAll: " + findAll);
-    }
-    
-    @Test(dependsOnMethods = "guardarConService")
-    public void testSpecification() {
-        Persona findOne = personaService.findOne(PacienteSpecs.byNombre("robert"));
-        assertNotNull(findOne);
-        System.out.println("FinOne Predicate: " + findOne);
-    }
-    
-    @Test(dependsOnMethods = "testSpecification")
-    public void testAlumno() {
-        Alumno a = new Alumno("Juan", "Barrionuevo");
-        a.setDocumento(new Documento("28188271", Documento.TipoDocumento.DNI));
-        a.setFechaNacimiento(Calendar.getInstance());
-        a = personaService.save(a);
-        assertNotNull(a);
-        assertFalse(a.isNew());
-    }
+//    @Test
+//    public void crearPaciente() {
+//        p = new Paciente("Maximiliano", "Barros");
+//        p.setDocumento(new Documento("34688417", Documento.TipoDocumento.DNI));
+//        HistoriaClinica hc= HistoriaClinica.createDefault();
+//        hc.setNumero(1);
+//        p.setHistoriaClinica(hc);
+//        p.setFechaNacimiento(Calendar.getInstance());
+////        p.setDomicilio(new Domicilio("Macaon 4123", "Cordoba"));
+//        personaService.save(p);
+//    }
+//
+//    @Test(dependsOnMethods = "crearPaciente")
+//    public void buscarPaciente() {
+//        Persona per = (Persona) personaService.findOne(PacienteSpecs.byNombre("Maximiliano"));
+//        assertNotNull(per);
+//        System.out.println("Nombre: " + per.getNombre());
+//        System.out.println("Apellido: " + per.getApellido());
+//    }
+//    
+//    @Test(dependsOnMethods = "buscarPaciente")
+//    public void guardarConService() {
+//        Paciente pa = new Paciente("Roberto", "Rodriguez");
+//        pa.setDocumento(new Documento("17267218", Documento.TipoDocumento.DNI));
+//        HistoriaClinica hc= HistoriaClinica.createDefault();
+//        hc.setNumero(2);
+//        pa.setHistoriaClinica(hc);
+////        pa.setDomicilio(new Domicilio("Ruelle 4123", "Cordoba"));
+//        pa.setFechaNacimiento(Calendar.getInstance());
+//        personaService.save(pa);
+//    }
+//    
+//    @Test(dependsOnMethods = "guardarConService")
+//    public void testCount() {
+//        Long count = personaService.count();
+//        assertTrue(count > 0);
+//        assertNotNull(count);
+//        System.out.println("Count: " + count);
+//    }
+//    
+//    @Test(dependsOnMethods = "guardarConService")
+//    public void testFindAll() {
+//        List<Persona> findAll = personaService.findAll();
+//        assertNotNull(findAll);
+//        assertFalse(findAll.isEmpty());
+//        System.out.println("FindAll: " + findAll);
+//    }
+//    
+//    @Test(dependsOnMethods = "guardarConService")
+//    public void testSpecification() {
+//        Persona findOne = personaService.findOne(PacienteSpecs.byNombre("robert"));
+//        assertNotNull(findOne);
+//        System.out.println("FinOne Predicate: " + findOne);
+//    }
+//    
+//    @Test(dependsOnMethods = "testSpecification")
+//    public void testAlumno() {
+//        Alumno a = new Alumno("Juan", "Barrionuevo");
+//        a.setDocumento(new Documento("28188271", Documento.TipoDocumento.DNI));
+//        a.setFechaNacimiento(Calendar.getInstance());
+//        a = personaService.save(a);
+//        assertNotNull(a);
+//        assertFalse(a.isNew());
+//    }
 }
