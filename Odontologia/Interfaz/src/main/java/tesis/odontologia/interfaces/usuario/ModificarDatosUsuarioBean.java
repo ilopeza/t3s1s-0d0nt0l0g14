80c8c6ec5c3914f7677b8d7a90145ceb209483ef
mau.g.sistemas <mau.g.sistemas@gmail.com>
2013-09-24 02:06:04 +0000

    Se agrega el metodo para modificar la contraseña del usuario

--- Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/usuario/ModificarDatosUsuarioBean.java
+++ Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/usuario/ModificarDatosUsuarioBean.java
@@ -28,6 +28,9 @@ public class ModificarDatosUsuarioBean {
     private UsuarioService usuarioService;
     private Usuario usuario;
     private Persona persona;
+    private String contraseñaActual;
+    private String nuevaContraseña;
+    private String repetirContraseña;
     
     @PostConstruct
     public void init() {
@@ -38,6 +41,28 @@ public class ModificarDatosUsuarioBean {
         usuario = login.getUsuario();
         persona = login.getPersona();
         
+    }
+    
+    
+    /**
+     * Método que se utiliza para cambiar la contraseña actual por otra.
+     */
+    public String cambiarContraseña()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+        LoginBean login =(LoginBean) session.getAttribute("loginBean");
+
+        if(contraseñaActual.compareTo(login.getPassword().toString())==0)
+        {
+            if(getNuevaContraseña().compareTo(getRepetirContraseña())==0)
+            {
+                usuario=login.getUsuario();
+                usuario.setContraseña(getNuevaContraseña());
+                getUsuarioService().save(usuario);
+            }
+        }
+        return "login";
     }
 
     /**
@@ -74,5 +99,56 @@ public class ModificarDatosUsuarioBean {
     public void setPersona(Persona persona) {
         this.persona = persona;
     }
+
+    /**
+     * @return the usuarioService
+     */
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+
+    /**
+     * @return the contraseñaActual
+     */
+    public String getContraseñaActual() {
+        return contraseñaActual;
+    }
+
+    /**
+     * @param contraseñaActual the contraseñaActual to set
+     */
+    public void setContraseñaActual(String contraseñaActual) {
+        this.contraseñaActual = contraseñaActual;
+    }
+
+    /**
+     * @return the nuevaContraseña
+     */
+    public String getNuevaContraseña() {
+        return nuevaContraseña;
+    }
+
+    /**
+     * @param nuevaContraseña the nuevaContraseña to set
+     */
+    public void setNuevaContraseña(String nuevaContraseña) {
+        this.nuevaContraseña = nuevaContraseña;
+    }
+
+    /**
+     * @return the repetirContraseña
+     */
+    public String getRepetirContraseña() {
+        return repetirContraseña;
+    }
+
+    /**
+     * @param repetirContraseña the repetirContraseña to set
+     */
+    public void setRepetirContraseña(String repetirContraseña) {
+        this.repetirContraseña = repetirContraseña;
+    }
+    
+    
     
 }
