583d2d8f2caddfcc4250a9b3ef8dfc28e6bce5bc
enzo.biancato@gmail.com <enzo.biancato@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-10-01 00:22:47 +0000

    Se terminó de programar el caso de uso "modificar datos de usuario". Se agregó opción al menú para ingresar a esta pagina.

--- trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/usuario/ModificarDatosUsuarioBean.java
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/usuario/ModificarDatosUsuarioBean.java
@@ -4,16 +4,20 @@
  */
 package tesis.odontologia.interfaces.usuario;
 
+import java.io.Serializable;
+import java.util.Calendar;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+import org.springframework.util.SerializationUtils;
 import tesis.odontologia.core.domain.Persona;
-import tesis.odontologia.core.domain.alumno.Alumno;
-import tesis.odontologia.core.domain.usuario.Usuario;
-import tesis.odontologia.core.service.UsuarioService;
+import tesis.odontologia.core.service.PersonaService;
+import tesis.odontologia.core.specification.PersonaSpecs;
+import tesis.odontologia.core.utils.FechaUtils;
 import tesis.odontologia.interfaces.login.LoginBean;
 
 /**
@@ -23,67 +27,67 @@ import tesis.odontologia.interfaces.login.LoginBean;
 @ManagedBean(name = "modificarDatosUsuarioBean")
 @ViewScoped
 public class ModificarDatosUsuarioBean {
-    
-    @ManagedProperty(value = "#{usuarioService}")
-    private UsuarioService usuarioService;
-    private Usuario usuario;
+
     private Persona persona;
+    @ManagedProperty(value = "#{personaService}")
+    private PersonaService personaService;
     private String contraseñaActual;
     private String nuevaContraseña;
     private String repetirContraseña;
-    
+    private LoginBean login;
+
     @PostConstruct
     public void init() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
-        LoginBean login =(LoginBean) session.getAttribute("loginBean");
+        login = (LoginBean) session.getAttribute("loginBean");
         
-        usuario = login.getUsuario();
-        persona = login.getPersona();
-        
-    }
-    
-    
-    /**
-     * Método que se utiliza para cambiar la contraseña actual por otra.
-     */
-    public String cambiarContraseña()
-    {
-        FacesContext context = FacesContext.getCurrentInstance();
-        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
-        LoginBean login =(LoginBean) session.getAttribute("loginBean");
+        persona = personaService.findOne(PersonaSpecs.byId(login.getPersona().getId()));
 
-        if(contraseñaActual.compareTo(login.getPassword().toString())==0)
-        {
-            if(getNuevaContraseña().compareTo(getRepetirContraseña())==0)
-            {
-                usuario=login.getUsuario();
-                usuario.setContraseña(getNuevaContraseña());
-                getUsuarioService().save(usuario);
-            }
+    }
+
+    public String guardarCambios() {
+        boolean error = false;
+        if ((contraseñaActual != null && !contraseñaActual.isEmpty()) && (nuevaContraseña == null || nuevaContraseña.isEmpty())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No puede asignarse una contraseña vacía.", null));
+            error = true;
         }
-        return "login";
+        if ((nuevaContraseña != null && !nuevaContraseña.isEmpty()) && (contraseñaActual == null || contraseñaActual.isEmpty())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Debe ingresar su contraseña actual.", null));
+            error = true;
+        }
+        if ((contraseñaActual != null && !contraseñaActual.isEmpty()) && !persona.getUsuario().getContraseña().equals(contraseñaActual)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "La contraseña actual no es correcta.", null));
+            error = true;
+        }
+        if ((nuevaContraseña != null && !nuevaContraseña.isEmpty()) && (!nuevaContraseña.equals(repetirContraseña)) || ((nuevaContraseña != null && !nuevaContraseña.isEmpty()) && (repetirContraseña == null || repetirContraseña.isEmpty()))) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Los campos 'Nueva contraseña' y 'Confirmar contraseña' deben coincidir.", null));
+            error = true;
+        }
+        if (error) {
+            return null;
+        }
+
+        try {
+            if (nuevaContraseña != null && !nuevaContraseña.isEmpty()) {
+                persona.getUsuario().setContraseña(nuevaContraseña);
+            }
+            persona = personaService.save(persona);
+           login.setPersona(persona);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Datos guardados correctamente", null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No se ha podido actualizar los datos.", null));
+        }
+
+        return "index";
     }
 
-    /**
-     * @param usuarioService the usuarioService to set
-     */
-    public void setUsuarioService(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
+    public String volverAInicio() {
+        return "index";
     }
 
-    /**
-     * @return the usuario
-     */
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    /**
-     * @param usuario the usuario to set
-     */
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public String formatFecha(Calendar c) {
+        return FechaUtils.fechaMaskFormat(c, "dd/MM/yyyy HH:mm");
     }
 
     /**
@@ -98,13 +102,6 @@ public class ModificarDatosUsuarioBean {
      */
     public void setPersona(Persona persona) {
         this.persona = persona;
-    }
-
-    /**
-     * @return the usuarioService
-     */
-    public UsuarioService getUsuarioService() {
-        return usuarioService;
     }
 
     /**
@@ -148,7 +145,11 @@ public class ModificarDatosUsuarioBean {
     public void setRepetirContraseña(String repetirContraseña) {
         this.repetirContraseña = repetirContraseña;
     }
-    
-    
-    
+
+    /**
+     * @param personaService the personaService to set
+     */
+    public void setPersonaService(PersonaService personaService) {
+        this.personaService = personaService;
+    }
 }
