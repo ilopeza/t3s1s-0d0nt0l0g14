b12d4d206db6df284a6ba0226695e9a5ac91fe26
aspesot <aspesot@gmail.com>
2013-10-14 23:36:00 +0000

    Se modificó AsignacionPaciente, Profesor,AsignacionPacienteSpecs, ProfesorSpecs y se agregó datos en Initialization

--- Odontologia/Core/src/main/java/tesis/odontologia/core/domain/profesor/Profesor.java
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/domain/profesor/Profesor.java
@@ -4,10 +4,21 @@
  */
 package tesis.odontologia.core.domain.profesor;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 import tesis.odontologia.core.domain.Persona;
+import tesis.odontologia.core.domain.materia.Materia;
 import tesis.odontologia.core.exception.GenericException;
+import tesis.odontologia.core.exception.MateriaException;
 
 /**
  *
@@ -16,17 +27,57 @@ import tesis.odontologia.core.exception.GenericException;
 @Entity
 @DiscriminatorValue(value = "PROFESOR")
 public class Profesor extends Persona {
+    
+    @ManyToMany
+    @JoinTable(name="profesorXmateria")
+    private List<Materia> materia;
 
+    
     public Profesor() {
+        materia= new ArrayList<Materia>();
     }
 
     public Profesor(String nombre, String apellido) {
         super(nombre, apellido);
     }
 
+    public boolean addMateria(Materia m) {
+        if (m == null) {
+            return false;
+        }        
+        return materia.add(m);
+    }
+
+    public boolean removeMateria(Materia m) {
+        if (m == null) {
+            return false;
+        }
+        return materia.remove(m);
+    }
+
+    public void removeAllMaterias() {
+        if (materia == null) {
+            return;
+        }
+        materia.clear();
+    }
+    public List<Materia> getMateria() {
+        return materia;
+    }
+
+    public void setMateria(List<Materia> materia) {
+        this.materia = materia;
+    }
+
     @Override
     public void validar() throws GenericException {
         super.validar();
+        if (materia == null) {
+            throw new MateriaException("El nombre de la catedra no puede ser nulo o vacio.");
+        }
+        for (Materia m : materia) {
+            m.validar();
+        }
     }
 
 }
