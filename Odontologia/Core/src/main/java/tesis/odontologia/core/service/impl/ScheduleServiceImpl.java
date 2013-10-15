3ac722cd4d0f495be5e341d1c4d8f63dbf4dad00
maxibarros <maxibarros@gmail.com>
2013-10-15 20:17:08 +0000

    Corregido error de inyeccion de dao en el servicio del scheduler.

--- Odontologia/Core/src/main/java/tesis/odontologia/core/service/impl/ScheduleServiceImpl.java
+++ Odontologia/Core/src/main/java/tesis/odontologia/core/service/impl/ScheduleServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import tesis.odontologia.core.dao.AsignacionPacienteDao;
 import tesis.odontologia.core.domain.asignaciones.AsignacionPaciente;
@@ -19,60 +20,30 @@ import tesis.odontologia.core.specification.AsignacionPacienteSpecs;
  * @author Mau
  */
 
-public class ScheduleServiceImpl{
+public class ScheduleServiceImpl {
 
-    private List<AsignacionPaciente> asignacionesAnalizadas = new ArrayList<AsignacionPaciente>();
-    private BooleanExpression predicate;
+    @Autowired
     private AsignacionPacienteDao asignacionPacienteDao;
     
 //    @Scheduled(cron = "*/15 * * * * ?")
 //    public void demoServiceMethod() {
 //        System.out.println("Method executed at every 5 seconds. Current time is :: " + new Date());
-//        //cargarAsignaciones();
-//        predicate = (AsignacionPacienteSpecs.byEstadoAsignacion(AsignacionPaciente.EstadoAsignacion.PENDIENTE));
+//        List<AsignacionPaciente> asigAnuladas = new ArrayList<AsignacionPaciente>();
+//        List<AsignacionPaciente> asignacionesBD = (List<AsignacionPaciente>) asignacionPacienteDao.findAll(AsignacionPacienteSpecs.byEstadoAsignacion(AsignacionPaciente.EstadoAsignacion.PENDIENTE));
 //
-//
-//        asignacionesAnalizadas = (List<AsignacionPaciente>) asignacionPacienteDao.findAll(predicate);
-//
-//        for (AsignacionPaciente asignacionAnalizada : asignacionesAnalizadas) {
+//        for (AsignacionPaciente asignacionAnalizada : asignacionesBD) {
 //            Calendar fechaActual = Calendar.getInstance();
 //
 //            if (fechaActual.get(Calendar.HOUR_OF_DAY) - asignacionAnalizada.getFechaAsignacion().get(Calendar.HOUR_OF_DAY) > 24) {
-//                asignacionAnalizada.setEstado(AsignacionPaciente.EstadoAsignacion.ANULADO);
+//                asignacionAnalizada.setEstado(AsignacionPaciente.EstadoAsignacion.ANULADA);
+//                asigAnuladas.add(asignacionAnalizada);
 //                //GUARDAR NUEVAMENTE LA ASIGNACION CON EL ESTADO ACTUALIZADO.
 //            }
 //        }
-//
+//        System.out.println("Actualizando asignaciones");
+//        asignacionPacienteDao.save(asigAnuladas);
+//        System.out.println("Asignaciones guardadas: " + asigAnuladas.toString());
 //        System.out.println("No se actualizaron asignaciones. Current time is: " + new Date());
-//
-//    }
+//}
 
-    /**
-     * @return the asignacionesAnalizadas
-     */
-    public List<AsignacionPaciente> getAsignacionesAnalizadas() {
-        return asignacionesAnalizadas;
     }
-
-    /**
-     * @param asignacionesAnalizadas the asignacionesAnalizadas to set
-     */
-    public void setAsignacionesAnalizadas(List<AsignacionPaciente> asignacionesAnalizadas) {
-        this.asignacionesAnalizadas = asignacionesAnalizadas;
-    }
-
-    /**
-     * @return the predicate
-     */
-    public BooleanExpression getPredicate() {
-        return predicate;
-    }
-
-    /**
-     * @param predicate the predicate to set
-     */
-    public void setPredicate(BooleanExpression predicate) {
-        this.predicate = predicate;
-    }
-
-}
