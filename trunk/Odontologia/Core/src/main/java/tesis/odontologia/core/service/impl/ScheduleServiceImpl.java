c16bf2d8a20ca20a1f075f5fde3b42be117d3560
mau.g.sistemas@gmail.com <mau.g.sistemas@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-10-15 22:09:05 +0000

    Se corrigen las fechas para poder compararlas correctamente.

--- trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/service/impl/ScheduleServiceImpl.java
+++ trunk/Odontologia/Core/src/main/java/tesis/odontologia/core/service/impl/ScheduleServiceImpl.java
@@ -4,10 +4,8 @@
  */
 package tesis.odontologia.core.service.impl;
 
-import com.mysema.query.types.expr.BooleanExpression;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,31 +17,37 @@ import tesis.odontologia.core.specification.AsignacionPacienteSpecs;
  *
  * @author Mau
  */
-
 public class ScheduleServiceImpl {
 
     @Autowired
     private AsignacionPacienteDao asignacionPacienteDao;
-    
-//    @Scheduled(cron = "*/15 * * * * ?")
-//    public void demoServiceMethod() {
-//        System.out.println("Method executed at every 5 seconds. Current time is :: " + new Date());
-//        List<AsignacionPaciente> asigAnuladas = new ArrayList<AsignacionPaciente>();
-//        List<AsignacionPaciente> asignacionesBD = (List<AsignacionPaciente>) asignacionPacienteDao.findAll(AsignacionPacienteSpecs.byEstadoAsignacion(AsignacionPaciente.EstadoAsignacion.PENDIENTE));
-//
-//        for (AsignacionPaciente asignacionAnalizada : asignacionesBD) {
-//            Calendar fechaActual = Calendar.getInstance();
-//
-//            if (fechaActual.get(Calendar.HOUR_OF_DAY) - asignacionAnalizada.getFechaAsignacion().get(Calendar.HOUR_OF_DAY) > 24) {
-//                asignacionAnalizada.setEstado(AsignacionPaciente.EstadoAsignacion.ANULADA);
-//                asigAnuladas.add(asignacionAnalizada);
-//                //GUARDAR NUEVAMENTE LA ASIGNACION CON EL ESTADO ACTUALIZADO.
-//            }
-//        }
-//        System.out.println("Actualizando asignaciones");
-//        asignacionPacienteDao.save(asigAnuladas);
-//        System.out.println("Asignaciones guardadas: " + asigAnuladas.toString());
-//        System.out.println("No se actualizaron asignaciones. Current time is: " + new Date());
-//}
 
+    /**
+     * Método que se ejecuta cada 6 horas. Busca todas las asignaciones en estado PENDIENTE
+     * y verifica que no hayan transcurrido mas de 24 horas desde la asignacion a la fecha actual. 
+     * En caso de que el tiempo sea mayor a 24 horas, se actualiza el estado de la asignacion al estado ANULADA.
+     */
+    @Scheduled(cron = "* */6 * * * ?")
+    public void demoServiceMethod() {
+        List<AsignacionPaciente> asigAnuladas = new ArrayList<AsignacionPaciente>();
+        List<AsignacionPaciente> asignacionesBD = (List<AsignacionPaciente>) asignacionPacienteDao.findAll(AsignacionPacienteSpecs.byEstadoAsignacion(AsignacionPaciente.EstadoAsignacion.PENDIENTE));
+
+        for (AsignacionPaciente asignacionAnalizada : asignacionesBD) {
+            Calendar fechaActual = Calendar.getInstance();
+            long milis1 = fechaActual.getTimeInMillis();
+            long milis2 = asignacionAnalizada.getFechaAsignacion().getTimeInMillis();
+
+            // calcula la diferencia en milisengundos
+            long diff = milis2 - milis1;
+
+            // calcula la diferencia en horas
+            long diffHours = diff / (60 * 60 * 1000);
+
+            if (Math.abs(diffHours) > 24) {
+                asignacionAnalizada.setEstado(AsignacionPaciente.EstadoAsignacion.ANULADA);
+                asigAnuladas.add(asignacionAnalizada);                
+            }
+        }        
+        asignacionPacienteDao.save(asigAnuladas);        
     }
+}
