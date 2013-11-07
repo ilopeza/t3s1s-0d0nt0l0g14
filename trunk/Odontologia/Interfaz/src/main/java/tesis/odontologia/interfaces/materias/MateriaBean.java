0ab316024b49c6bca34d463ca0407962095d0561
mau.g.sistemas@gmail.com <mau.g.sistemas@gmail.com@211623fc-902e-278b-7327-a167b177650e>
2013-11-07 00:24:36 +0000

    Se unifica todo lo de materia con lo de trabajo practico. Se guarda, modifica y elimina un trabajo practico para una determinada materia

--- trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/materias/MateriaBean.java
+++ trunk/Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/materias/MateriaBean.java
@@ -4,16 +4,23 @@
  */
 package tesis.odontologia.interfaces.materias;
 
+import com.mysema.query.types.expr.BooleanExpression;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import tesis.odontologia.core.domain.materia.Catedra;
 import tesis.odontologia.core.domain.materia.Materia;
+import tesis.odontologia.core.domain.materia.TrabajoPractico;
 import tesis.odontologia.core.service.CatedraService;
 import tesis.odontologia.core.service.MateriaService;
+import tesis.odontologia.core.service.TrabajoPracticoService;
+import tesis.odontologia.core.specification.MateriaSpecs;
+import tesis.odontologia.interfaces.validacion.Validacion;
 
 /**
  *
@@ -26,31 +33,129 @@ public class MateriaBean {
     /**
      * Creates a new instance of MateriasBean
      */
+    private TrabajoPractico trabajoPractico;
+    private TrabajoPractico selectedTrabajoPractico;
+    private List<TrabajoPractico> trabajosPracticos;
+    private List<TrabajoPractico> trabajosPracticosMateria;
+    private Validacion validacion = new Validacion();
     private List<Materia> materias;
     private List<Catedra> catedras;
     private Materia materia;
     private Catedra catedra;
     private Materia selectedMateria;
     private Catedra selectedCatedra;
-    
     @ManagedProperty(value = "#{materiaService}")
     private MateriaService materiaService;
+    @ManagedProperty(value = "#{trabajoPracticoService}")
+    private TrabajoPracticoService trabajoPracticoService;
     @ManagedProperty(value = "#{catedraService}")
     private CatedraService catedraService;
+    private boolean rendered = true;
+    private boolean nuevoTPHabilitado;
+    private boolean modificarTPHabilitado;
 
     @PostConstruct
     public void init() {
         materias = materiaService.findAll();
         catedras = catedraService.findAll();
+        trabajosPracticos = new ArrayList<TrabajoPractico>();        
         resetFields();
+        resetFieldsTP();
     }
-    
+
     public void resetFields() {
         materia = new Materia();
         catedra = new Catedra();
     }
 
+    public void resetFieldsTP() {
+        trabajoPractico = new TrabajoPractico();
+    }
+    
+    public void nuevoTrabajoPractico(){
+        trabajoPractico = new TrabajoPractico();
+        nuevoTPHabilitado = true;
+    }
+
     public MateriaBean() {
+    }
+
+    public void modificarTrabajoPractico() {
+        trabajoPractico = selectedTrabajoPractico;
+        nuevoTPHabilitado = true;
+        //retornarMateria();
+
+    }
+
+    public void eliminarTrabajoPractico() {
+        trabajoPractico = selectedTrabajoPractico;
+        materia = materiaService.reload(materia, 1);
+        //materia.removeTrabajoPractico(trabajoPractico);
+        materia.getTrabajoPractico().remove(trabajoPractico);
+        materia = materiaService.save(materia);
+        trabajoPractico = new TrabajoPractico();
+        
+    }
+
+    public void saveTrabajoPractico() {
+
+        //if (validar())
+        if (trabajoPractico.isNew()) {
+            materia = materiaService.reload(materia, 1);
+            //materia.addTrabajoPractico(trabajoPractico);
+            materia.getTrabajoPractico().add(trabajoPractico);            
+            materia = materiaService.save(materia);
+            materia = new Materia();
+            resetFieldsTP();
+            nuevoTPHabilitado = false;
+        }else{
+            trabajoPractico = trabajoPracticoService.save(trabajoPractico);
+            trabajoPractico = new TrabajoPractico();
+            nuevoTPHabilitado = false;
+        }   
+    }
+
+
+    public void retornarMateria() {
+
+        BooleanExpression predicate = null;
+        predicate = (MateriaSpecs.byTrabajoPractico(trabajoPractico));
+
+        materia = materiaService.findOne(predicate);
+
+        //return materia.getNombre();
+    }
+
+    private boolean validar() {
+        boolean varValidacion = true;
+
+        if (!validacion.validarTexto(trabajoPractico.getNombre())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Campo nombre de Trabajo Práctico debe ser texto", null));
+            varValidacion = false;
+        }
+
+        if (validacion.nullEmpty(trabajoPractico.getDescripcion())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Por favor ingrese una descripción", null));
+            varValidacion = false;
+        }
+
+        return varValidacion;
+    }
+
+    public void buscarTPs() {
+
+        //materias = buscarMaterias();
+        trabajosPracticos = buscarTrabajosPracticos();
+    }
+
+    private List<Materia> buscarMaterias() {
+        return materiaService.findAll();
+    }
+
+    private List<TrabajoPractico> buscarTrabajosPracticos() {
+        //return trabajoPracticoService.findAll();
+        materia = materiaService.reload(materia, 1);
+        return materia.getTrabajoPractico();
     }
 
     public void saveMateria() {
@@ -64,7 +169,6 @@ public class MateriaBean {
     }
 
     //GETTERS AND SETTERS
-    
     public List<Catedra> getCatedras() {
         return catedras;
     }
@@ -127,5 +231,131 @@ public class MateriaBean {
 
     public void setCatedraService(CatedraService catedraService) {
         this.catedraService = catedraService;
+    }
+
+    /**
+     * @return the trabajoPractico
+     */
+    public TrabajoPractico getTrabajoPractico() {
+        return trabajoPractico;
+    }
+
+    /**
+     * @param trabajoPractico the trabajoPractico to set
+     */
+    public void setTrabajoPractico(TrabajoPractico trabajoPractico) {
+        this.trabajoPractico = trabajoPractico;
+    }
+
+    /**
+     * @return the selectedTrabajoPractico
+     */
+    public TrabajoPractico getSelectedTrabajoPractico() {
+        return selectedTrabajoPractico;
+    }
+
+    /**
+     * @param selectedTrabajoPractico the selectedTrabajoPractico to set
+     */
+    public void setSelectedTrabajoPractico(TrabajoPractico selectedTrabajoPractico) {
+        this.selectedTrabajoPractico = selectedTrabajoPractico;
+    }
+
+    /**
+     * @return the trabajosPracticos
+     */
+    public List<TrabajoPractico> getTrabajosPracticos() {
+        return trabajosPracticos;
+    }
+
+    /**
+     * @param trabajosPracticos the trabajosPracticos to set
+     */
+    public void setTrabajosPracticos(List<TrabajoPractico> trabajosPracticos) {
+        this.trabajosPracticos = trabajosPracticos;
+    }
+
+    /**
+     * @return the trabajosPracticosMateria
+     */
+    public List<TrabajoPractico> getTrabajosPracticosMateria() {
+        return trabajosPracticosMateria;
+    }
+
+    /**
+     * @param trabajosPracticosMateria the trabajosPracticosMateria to set
+     */
+    public void setTrabajosPracticosMateria(List<TrabajoPractico> trabajosPracticosMateria) {
+        this.trabajosPracticosMateria = trabajosPracticosMateria;
+    }
+
+    /**
+     * @return the validacion
+     */
+    public Validacion getValidacion() {
+        return validacion;
+    }
+
+    /**
+     * @param validacion the validacion to set
+     */
+    public void setValidacion(Validacion validacion) {
+        this.validacion = validacion;
+    }
+
+    /**
+     * @return the trabajoPracticoService
+     */
+    public TrabajoPracticoService getTrabajoPracticoService() {
+        return trabajoPracticoService;
+    }
+
+    /**
+     * @param trabajoPracticoService the trabajoPracticoService to set
+     */
+    public void setTrabajoPracticoService(TrabajoPracticoService trabajoPracticoService) {
+        this.trabajoPracticoService = trabajoPracticoService;
+    }
+
+    /**
+     * @return the rendered
+     */
+    public boolean isRendered() {
+        return rendered;
+    }
+
+    /**
+     * @param rendered the rendered to set
+     */
+    public void setRendered(boolean rendered) {
+        this.rendered = rendered;
+    }
+
+    /**
+     * @return the nuevoTPHabilitado
+     */
+    public boolean isNuevoTPHabilitado() {
+        return nuevoTPHabilitado;
+    }
+
+    /**
+     * @param nuevoTPHabilitado the nuevoTPHabilitado to set
+     */
+    public void setNuevoTPHabilitado(boolean nuevoTPHabilitado) {
+        this.nuevoTPHabilitado = nuevoTPHabilitado;
+    }
+
+    /**
+     * @return the modificarTPHabilitado
+     */
+    public boolean isModificarTPHabilitado() {
+        return modificarTPHabilitado;
+    }
+
+    /**
+     * @param modificarTPHabilitado the modificarTPHabilitado to set
+     */
+    public void setModificarTPHabilitado(boolean modificarTPHabilitado) {
+        this.modificarTPHabilitado = modificarTPHabilitado;
     }
 }
