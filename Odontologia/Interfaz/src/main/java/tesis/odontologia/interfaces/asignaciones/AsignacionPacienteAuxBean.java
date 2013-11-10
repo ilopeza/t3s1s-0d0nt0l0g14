e52f28a16af527327405342ccc6ab2dbe036a9c3
ilopezarzuaga <ilopezarzuaga@gmail.com>
2013-11-10 04:19:08 +0000

    Normalización interfaces.

--- Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/asignaciones/AsignacionPacienteAuxBean.java
+++ Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/asignaciones/AsignacionPacienteAuxBean.java
@@ -18,98 +18,99 @@ import tesis.odontologia.core.utils.FechaUtils;
 public class AsignacionPacienteAuxBean {
 
     private String paciente;
-        private String alumno;
-        private String fecha;
-        private String descripcionDiagnostico;
-        private String materia;
-        private String catedra;
-        private Long id;
-        private String trabajoPractico;
-        @ManagedProperty(value = "#{materiaService}")
-        private MateriaService materiaService;
-        private Materia mat;
-        
-        public AsignacionPacienteAuxBean(AsignacionPaciente asignacionPaciente) {
-            this.id = asignacionPaciente.getId();
-            this.paciente = asignacionPaciente.getPaciente().toString();
-            this.alumno = asignacionPaciente.getAlumno().toString();
-            this.fecha = FechaUtils.fechaMaskFormat(asignacionPaciente.getFechaAsignacion(), "dd/MM/yyyy HH:mm");
-            this.descripcionDiagnostico = asignacionPaciente.getDiagnostico().getDescripcion();
-            mat=materiaService.findOne(MateriaSpecs.byCatedra(asignacionPaciente.getCatedra()));
-            this.materia=mat.getNombre();
-            this.catedra = asignacionPaciente.getCatedra().toString();
-            this.trabajoPractico = asignacionPaciente.getDiagnostico().getTrabajoPractico().getNombre();
-        }
+    private String alumno;
+    private String fecha;
+    private String descripcionDiagnostico;
+    private String materia;
+    private String catedra;
+    private Long id;
+    private String trabajoPractico;
+    @ManagedProperty(value = "#{materiaService}")
+    private MateriaService materiaService;
+    private Materia mat;
 
-        // GETTERS Y SETTERS
-                public MateriaService getMateriaService() {
-                    return materiaService;
-                }
-        
-                public void setMateriaService(MateriaService materiaService) {
-                    this.materiaService = materiaService;
-                }
-                  public String getMateria() {
-                    return materia;
-                }
-        
-                public void setMateria(String materia) {
-                    this.materia = materia;
-                }
-        public Long getId() {
-            return id;
-        }
+    public AsignacionPacienteAuxBean(AsignacionPaciente asignacionPaciente) {
+        this.id = asignacionPaciente.getId();
+        this.paciente = asignacionPaciente.getPaciente().toString();
+        this.alumno = asignacionPaciente.getAlumno().toString();
+        this.fecha = FechaUtils.fechaMaskFormat(asignacionPaciente.getFechaAsignacion(), "dd/MM/yyyy HH:mm");
+        this.descripcionDiagnostico = asignacionPaciente.getDiagnostico().getDescripcion();
+        mat = materiaService.findOne(MateriaSpecs.byCatedra(asignacionPaciente.getCatedra()));
+        this.materia = mat.getNombre();
+        this.catedra = asignacionPaciente.getCatedra().toString();
+        this.trabajoPractico = asignacionPaciente.getDiagnostico().getTrabajoPractico().getNombre();
+    }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    // GETTERS Y SETTERS
+    public MateriaService getMateriaService() {
+        return materiaService;
+    }
 
-        public String getDescripcionDiagnostico() {
-            return descripcionDiagnostico;
-        }
+    public void setMateriaService(MateriaService materiaService) {
+        this.materiaService = materiaService;
+    }
 
-        public void setDescripcionDiagnostico(String descripcionDiagnostico) {
-            this.descripcionDiagnostico = descripcionDiagnostico;
-        }
+    public String getMateria() {
+        return materia;
+    }
 
-        public String getTrabajoPractico() {
-            return trabajoPractico;
-        }
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
 
-        public void setTrabajoPractico(String trabajoPractico) {
-            this.trabajoPractico = trabajoPractico;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public String getCatedra() {
-            return catedra;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public void setCatedra(String catedra) {
-            this.catedra = catedra;
-        }
+    public String getDescripcionDiagnostico() {
+        return descripcionDiagnostico;
+    }
 
-        public String getPaciente() {
-            return paciente;
-        }
+    public void setDescripcionDiagnostico(String descripcionDiagnostico) {
+        this.descripcionDiagnostico = descripcionDiagnostico;
+    }
 
-        public void setPaciente(String paciente) {
-            this.paciente = paciente;
-        }
+    public String getTrabajoPractico() {
+        return trabajoPractico;
+    }
 
-        public String getAlumno() {
-            return alumno;
-        }
+    public void setTrabajoPractico(String trabajoPractico) {
+        this.trabajoPractico = trabajoPractico;
+    }
 
-        public void setAlumno(String alumno) {
-            this.alumno = alumno;
-        }
+    public String getCatedra() {
+        return catedra;
+    }
 
-        public String getFecha() {
-            return fecha;
-        }
+    public void setCatedra(String catedra) {
+        this.catedra = catedra;
+    }
 
-        public void setFecha(String fecha) {
-            this.fecha = fecha;
-        }
+    public String getPaciente() {
+        return paciente;
+    }
 
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(String alumno) {
+        this.alumno = alumno;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 }
