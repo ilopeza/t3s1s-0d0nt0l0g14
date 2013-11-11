fde893308b706e03b157aa6a3dfe6b7a8812b346
maxibarros <maxibarros@gmail.com>
2013-11-11 05:27:46 +0000

    Se agregaron comentarios sobre los metodos agregados a la clase.

--- Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/Web.java
+++ Odontologia/Interfaz/src/main/java/tesis/odontologia/interfaces/Web.java
@@ -18,16 +18,34 @@ import tesis.odontologia.interfaces.login.LoginBean;
  */
 public class Web {
     
+    /**
+     * Metodo que recarga la pagina en la cual se encuentra uno segun la URL del 
+     * navegador. Vuelve a ejecutar los metodos marcados con @PostConstruct como
+     * por ejemplo el metodo Init().
+     * @throws IOException 
+     */
     public static void reloadPage() throws IOException {
         ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
     }
     
+    /**
+     * Metodo que ejecuta acciones sobre un dialog desde el bean, permitiendo mostrarlo
+     * u ocultarlo desde el bean. Se debe pasar por parametro el nombre del dialogo
+     * junto a la accion. 
+     * Ejemplo: nombreDialog.show() --- nombreDialog.hide()
+     * @param dialog 
+     */
     public static void callDialog(String dialog) {
         RequestContext context = RequestContext.getCurrentInstance();
         context.execute(dialog);
     }
     
+    /**
+     * Metodo que retorna el bean del login. En el mismo siempre se encuentra 
+     * cargada la persona logueada.
+     * @return 
+     */
     public static LoginBean getLoginBean() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
